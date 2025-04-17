ac3e4846e8c3ea2ecb22a35147914e74fca8db4c
coder-supratim <supratim.arya@gmail.com>
2025-04-16 21:18:18 -0400

    Added open ai and vector db configs

--- src/main/java/com/timsoft/ai/service/DataLoaderService.java
+++ src/main/java/com/timsoft/ai/service/DataLoaderService.java
@@ -7,7 +7,7 @@ import org.springframework.ai.reader.pdf.PagePdfDocumentReader;
 import org.springframework.ai.reader.pdf.config.PdfDocumentReaderConfig;
 import org.springframework.ai.transformer.splitter.TokenTextSplitter;
 import org.springframework.ai.vectorstore.VectorStore;
-import org.springframework.ai.vectorstore.redis.RedisVectorStore;
+import org.springframework.ai.vectorstore.RedisVectorStore;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
